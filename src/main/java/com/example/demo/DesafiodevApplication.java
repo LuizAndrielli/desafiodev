2f7ce99d2981f89dce699d8f08d7b7ec22122eee
LuizAndrielli <r4t0X@hotmail.com>
2022-09-12 11:27:10 -0300

    Projeto criado

---
+++ src/main/java/com/example/demo/DesafiodevApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiodevApplication.class, args);
+	}
+
+}
